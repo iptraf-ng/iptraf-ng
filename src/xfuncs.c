312f6b01a46f2d6a5e006a42fd0d6f512e1be8e1
Nikola Pajkovsky <npajkovs@redhat.com>
2010-07-07 18:09:10 +0200

    new xfuncs module. each funtion die when fails
    Signed-off-by: Nikola Pajkovsky <npajkovs@redhat.com>

---
+++ src/xfuncs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "xfuncs.h"
+
+
+
+//#define NORETURN __attribute__ ((noreturn))
+static void die_out_of_memory()
+{
+    exit(EXIT_FAILURE);
+}
+
+// Die if we can't allocate size bytes of memory.
+void* xmalloc(size_t size)
+{
+    void *ptr = malloc(size);
+    if (ptr == NULL && size != 0)
+        die_out_of_memory();
+    return ptr;
+}
+
+
