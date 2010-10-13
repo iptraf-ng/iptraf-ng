a11eed4de5516c2c17e1c0531fee7992cf85fd6b
Nikola Pajkovsky <npajkovs@redhat.com>
2010-10-13 11:56:05 +0200

    fix indentaion in license
    Signed-off-by: Nikola Pajkovsky <npajkovs@redhat.com>

--- src/iptraf-ng-compat.h
+++ src/iptraf-ng-compat.h
@@ -2,13 +2,12 @@
     This software is open-source; you may redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+    (at your option) any later version.
 
-This program is distributed WITHOUT ANY WARRANTY; without even the
-implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License in the included COPYING file for
-details.
-
+    This program is distributed WITHOUT ANY WARRANTY; without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License in the included COPYING file for
+    details.
 */
 
 #ifndef IPTRAF_NG_COMPAT_H
