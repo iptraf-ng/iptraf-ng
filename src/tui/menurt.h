d1c561c1e640795501d06e057bc61a6a8571d9a4
Nikola Pajkovsky <npajkovs@redhat.com>
2011-02-22 16:18:32 +0100

    menu.h: fix indentaion
    Signed-off-by: Nikola Pajkovsky <npajkovs@redhat.com>

--- src/tui/menurt.h
+++ src/tui/menurt.h
@@ -15,38 +15,38 @@
 #define SHORTCUTSTRLEN_MAX 25
 
 struct ITEM {
-    char option[OPTIONSTRLEN_MAX];
-    char desc[DESCSTRLEN_MAX];
-    unsigned int itemtype;
-    struct ITEM *prev;
-    struct ITEM *next;
+	char option[OPTIONSTRLEN_MAX];
+	char desc[DESCSTRLEN_MAX];
+	unsigned int itemtype;
+	struct ITEM *prev;
+	struct ITEM *next;
 };
 
 struct MENU {
-    struct ITEM *itemlist;
-    struct ITEM *selecteditem;
-    struct ITEM *lastitem;
-    int itemcount;
-    int postn;
-    int x1, y1;
-    int x2, y2;
-    unsigned int menu_maxx;
-    WINDOW *menuwin;
-    PANEL *menupanel;
-    WINDOW *descwin;
-    PANEL *descpanel;
-    int borderattr;
-    int normalattr;
-    int highattr;
-    int barnormalattr;
-    int barhighattr;
-    int descriptionattr;
-    char shortcuts[SHORTCUTSTRLEN_MAX];
+	struct ITEM *itemlist;
+	struct ITEM *selecteditem;
+	struct ITEM *lastitem;
+	int itemcount;
+	int postn;
+	int x1, y1;
+	int x2, y2;
+	unsigned int menu_maxx;
+	WINDOW *menuwin;
+	PANEL *menupanel;
+	WINDOW *descwin;
+	PANEL *descpanel;
+	int borderattr;
+	int normalattr;
+	int highattr;
+	int barnormalattr;
+	int barhighattr;
+	int descriptionattr;
+	char shortcuts[SHORTCUTSTRLEN_MAX];
 };
 
 extern void tx_initmenu(struct MENU *menu, int y1, int x1, int y2, int x2,
-    int borderattr, int normalattr, int highattr,
-    int barnormalattr, int barhighattr, int descattr);
+			int borderattr, int normalattr, int highattr,
+			int barnormalattr, int barhighattr, int descattr);
 extern void tx_additem(struct MENU *menu, char *item, char *desc);
 extern void tx_showitem(struct MENU *menu, struct ITEM *itemptr, int selected);
 extern void tx_showmenu(struct MENU *menu);
