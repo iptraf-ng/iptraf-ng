5ef5dd1adb22807b2696e1b14e00da9db9c7e5c8
Nikola Pajkovsky <npajkovs@redhat.com>
2011-03-01 10:46:46 +0100

    new ip checksum based on rfc1071 implementaion.
    http://www.faqs.org/rfcs/rfc1071.html. See section 4.1 "C".
    It's not fully copy&paste becouse they have a little typo in code
    
    Signed-off-by: Nikola Pajkovsky <npajkovs@redhat.com>

--- src/ipcsum.c
+++ src/ipcsum.c
@@ -1,43 +1,38 @@
-
-
 /*
- * in_cksum --
- *      Checksum routine for Internet Protocol family headers (C Version)
- *
- *      borrowed from the ping program from the Linux NetKit.  
- *      This is a standard C version of the
- *      IP header checksum calculation algorithm.
- */
+    Copyright (C) 2010  Nikola Pajkovsky (npajkovs@redhat.com)
 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-// move it to iptraf-ng-compat?
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include "iptraf-ng-compat.h"
 
-int in_cksum(u_short * addr, int len)
+/* code taken from http://www.faqs.org/rfcs/rfc1071.html. See section 4.1 "C"  */
+int in_cksum(u_short *addr, int len)
 {
-    register int nleft = len;
-    register u_short *w = addr;
-    register int sum = 0;
-    u_short answer = 0;
+	register int sum = 0;
 
-    /*
-     * Our algorithm is simple, using a 32 bit accumulator (sum), we add
-     * sequential 16 bit words to it, and at the end, fold back all the
-     * carry bits from the top 16 bits into the lower 16 bits.
-     */
-    while (nleft > 1) {
-        sum += *w++;
-        nleft -= 2;
-    }
+        while (len > 1) {
+		sum += *(u_short *)addr++;
+		len -= 2;
+	}
 
-    /* mop up an odd byte, if necessary */
-    if (nleft == 1) {
-        *(u_char *) (&answer) = *(u_char *) w;
-        sum += answer;
-    }
-    /* add back carry outs from top 16 bits to low 16 bits */
-    sum = (sum >> 16) + (sum & 0xffff); /* add hi 16 to low 16 */
-    sum += (sum >> 16);         /* add carry */
-    answer = ~sum;              /* truncate to 16 bits */
-    return (answer);
+	if (len > 1)
+		sum += *(unsigned char *) addr;
+
+	while (sum >> 16)
+		sum = (sum & 0xffff) + (sum >> 16);
+
+	return (u_short)(~sum);
 }
